3f80ef74b23e60e41a162001813763ed9ba4b13e
Oğuz Vuruşkaner <oguz@localhost.localdomain>
2019-02-28 21:54:48 +0300

    Fixed bugs in main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from Photo import Photo
 from Slide import Slide
 
+import sys
+
 def combineVertical(photos):
     vertLists = []
     for i in range(len(photos)):
         if not photos[i].horizonality:
             vertLists.append(photos[i])
     pairMatrix = []
-    for i in range(len(photos)):
+    for i in range(len(vertLists)):
         temp = []
-        for j in range(len(photos)):
+        for j in range(len(vertLists)):
             temp.append(-1)
         pairMatrix.append(temp)
-
     for i in range(len(pairMatrix)):
         for j in range(i + 1, len(pairMatrix)):
             if i == j:
                 continue
             pairMatrix[i][j] = getNumberOfTags(vertLists[i], vertLists[j])
+    return pairMatrix
 
 
 
@@ -28,12 +30,25 @@ def getNumberOfTags(p1, p2):
             tags.append(p2.tags[i])
     return len(tags)
 
+def printPairs(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            sys.stdout.write(str(matrix[i][j])+ " ")
+        print ()
+
+
+def generateSlides(photos) -> [Slide]:
+
+    verticals = combineVertical(photos)
+    horitzonalList = map(lambda x : x.horitzonality ,photos)
+
+    return [0]
 
 def main(filename):
 
     photos = Photo.takeInput(filename)
-    a = Slide(photos[0])    
-    b = Slide([photos[2],photos[3]])
+    slides = generateSlides(photos)
+    Slide.printSlides(slides)
 
 
 if __name__ == "__main__":
