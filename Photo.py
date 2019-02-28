35d239e3e9fba136dfcaa9fa5526120dbdd88200
Oğuz Vuruşkaner <oguz@localhost.localdomain>
2019-02-28 21:19:53 +0300

    Added id to photo

--- Photo.py
+++ Photo.py
@@ -4,7 +4,7 @@ class Photo:
     def __init__(self,props):
 
         self.horizonality = props["horizonality"]
-
+        self.id = props["id"]
         self.tags = props["tags"]
 
     @staticmethod
@@ -30,6 +30,7 @@ class Photo:
                     words = content[index].split()
                     curPhoto["horizonality"] = True if words[0] == "H" else False
                     curPhoto["tags"] = []
+                    curPhoto["id"] = index-1
                     numberOfTags = int(words[1])
                     for i in range(numberOfTags):
                         curPhoto["tags"].append(words[i+2])
